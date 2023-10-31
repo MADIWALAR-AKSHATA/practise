ddc26d5681cc20de3f2b260da926227af55bc0c5
Akshata <akshatamadiwalar123@gmail.com>
2023-10-31 22:57:43 +0530

    added functionality to button

---
+++ app.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click", function(){
+    let p = document.querySelector("p");
+    p.innerText = "Learn Full Stack Development";
+    p.style.color = "green";
+    // console.log("hii");
+})
